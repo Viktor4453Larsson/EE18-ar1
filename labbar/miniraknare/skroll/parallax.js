d2463df5319f425784248715cda783395b228ec8
Viktor Larsson <viktor.larsson8@learnet.se>
2020-05-08 15:46:14 +0200

    små fixar

--- labbar/miniraknare/skroll/parallax.js
+++ labbar/miniraknare/skroll/parallax.js
@@ -4,9 +4,9 @@ window.addEventListener("scroll", function(e) {
     /* Hur mycket skrollar vi? */
 
     eTargets.forEach(function(element) {
-        
         var rate = element.dataset.rate;
+        console.log(rate);
 
-        element.style.transform = "translate3d(" + distance + "px, " + window.pageYOffset + "px, 0px)";
-})
-})
+        element.style.transform = "translateY(" + window.pageYOffset * rate + "px)";
+    });
+});
